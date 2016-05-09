b298336ada9299aee05106f4d0f9ac5e0a1cb190
Robin Nabel <robin.nabel@gmail.com>
2016-05-09 17:19:21 +0100

    Initial commit

---
+++ scripts/background.js
@@ -0,0 +1,17 @@
+function cookieHandler(info) {
+    if (info.cause == "explicit" && info.cookie.domain == "8tracks" && info.cookie.name == "country_code2") {
+        // Overwrite cookie.
+        var replacementCookie = info.cookie;
+        replacementCookie.value = "US";
+        chrome.cookies.set(replacementCookie);
+    }
+}
+
+chrome.cookies.onChanged.addListener(function (info) {
+    cookieHandler(info);
+    console.log("onChanged" + JSON.stringify(info));
+});
+
+// Delete existing cookie on startup.
+chrome.cookies.remove({name: "country_code2", url: "http://8tracks.com"});
+chrome.cookies.remove({name: "country_code2", url: "https://8tracks.com"});
---
+++ scripts/contentScript.js
@@ -0,0 +1,12 @@
+// Taken from: http://stackoverflow.com/a/30387077/3918512
+function removeItem(sKey, sPath, sDomain) {
+    document.cookie = encodeURIComponent(sKey) +
+        "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+        (sDomain ? "; domain=" + sDomain : "") +
+        (sPath ? "; path=" + sPath : "");
+}
+
+removeItem("country_code2");
+
+// Set the cookie to US.
+document.cookie="country_code2=US";
